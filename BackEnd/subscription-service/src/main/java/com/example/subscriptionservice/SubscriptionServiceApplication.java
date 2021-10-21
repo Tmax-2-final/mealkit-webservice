e4c65f73133b73ebaec9fe9404ce53e67ad5b6a6
ojiwoong <ojiwoong@gmail.com>
2021-10-21 09:29:24 +0900

    구독,주문 RDS 비밀번호 변경

--- BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/SubscriptionServiceApplication.java
+++ BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/SubscriptionServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.subscriptionservice;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,5 +13,20 @@ public class SubscriptionServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SubscriptionServiceApplication.class, args);
     }
-
 }
+
+// 프로젝트 실행 후 암호화 된 비밀번호 출력
+//public class SubscriptionServiceApplication implements CommandLineRunner {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SubscriptionServiceApplication.class, args);
+//    }
+//
+//    public void run(String... args) throws Exception {
+//        StandardPBEStringEncryptor spe = new StandardPBEStringEncryptor();
+//        spe.setAlgorithm("PBEWithMD5AndDES");
+//        spe.setPassword("암호화비밀번호");
+//        System.out.println("db username = " + spe.encrypt("디비유저명"));
+//        System.out.println("db password = " + spe.encrypt("디비유저비밀번호"));
+//    }
+//}
