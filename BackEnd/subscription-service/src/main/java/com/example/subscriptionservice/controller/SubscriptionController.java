922e241c51b0ec55983c5a17def763d6f5c9af32
ojiwoong <ojiwoong@gmail.com>
2021-10-25 18:18:08 +0900

    구독 CRUD API 구현

--- BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/controller/SubscriptionController.java
+++ BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/controller/SubscriptionController.java
@@ -1,23 +1,22 @@
 package com.example.subscriptionservice.controller;
 
+import com.example.subscriptionservice.dto.SubscriptionDto;
 import com.example.subscriptionservice.entity.SubscriptionGradeEntity;
 import com.example.subscriptionservice.service.SubscriptionService;
-import com.example.subscriptionservice.vo.ResponseSubscriptionGrade;
+import com.example.subscriptionservice.vo.*;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -60,6 +59,101 @@ public class SubscriptionController {
         });
 
         log.info("구독등급 조회 API END");
+
         return ResponseEntity.status(HttpStatus.OK).body(responseSubscriptionGradeList);
+    }
+
+//    @ApiOperation(value = "특정회원 구독등급 조회", notes = "특정회원의 구독등급을 조회한다.")
+//    @GetMapping("/subscription/grade/{userId}")
+//    public ResponseEntity<ResponseSubscriptionGrade> getUserSubscriptionGrade(@PathVariable("userId") String userId) {
+//        log.info("특정회원 구독등급 조회 API START");
+//
+//        ModelMapper mapper = new ModelMapper();
+//        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//
+//        SubscriptionDto subscriptionDto = subscriptionService.getSubscription(userId);
+//
+//        SubscriptionGradeEntity subscriptionGradeEntity = subscriptionService.getSubscriptionGrade(subscriptionDto.getSubGradeId());
+//
+//        ResponseSubscriptionGrade responseSubscriptionGrade = mapper.map(subscriptionGradeEntity, ResponseSubscriptionGrade.class);
+//
+//        log.info("특정회원 구독등급 조회 API END");
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(responseSubscriptionGrade);
+//    }
+
+    @ApiOperation(value = "구독 등록", notes = "구독 신청한 회원의 구독을 등록한다.")
+    @PostMapping(value = "/subscription")
+    public ResponseEntity<ResponseSubscription> createSubscription(@RequestBody RequestSubscription subscription){
+        log.info("구독 등록 API START");
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 엄격한 매칭
+        // request -> dto
+        SubscriptionDto subscriptionDto = mapper.map(subscription, SubscriptionDto.class); // userDto -> userEntity 로 매핑
+
+        subscriptionDto = subscriptionService.createSubscription(subscriptionDto);
+        // dto -> response
+        ResponseSubscription responseSubscription = mapper.map(subscriptionDto, ResponseSubscription.class);
+
+        log.info("구독 등록 API END");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseSubscription);
+    }
+
+    @ApiOperation(value = "구독 변경", notes = "회원의 구독을 변경한다.")
+    @PutMapping(value = "/subscription")
+    public ResponseEntity<String> updateSubscription(@RequestBody RequestUpdateSubscription requestUpdateSubscription){
+        log.info("구독 변경 API START");
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 엄격한 매칭
+
+        SubscriptionDto subscriptionDto = subscriptionService.getSubscription(requestUpdateSubscription.getUserId());
+
+        subscriptionDto.setSubGradeId(requestUpdateSubscription.getSubGradeId());
+
+        subscriptionService.updateSubscription(subscriptionDto);
+
+        log.info("구독 변경 API END");
+
+        return ResponseEntity.status(HttpStatus.OK).body("구독변경 완료");
+    }
+
+    @ApiOperation(value = "구독 취소", notes = "회원의 구독상태를 구독취소로 변경한다. (1. 구독중, 2. 구독취소)")
+    @DeleteMapping(value = "/subscription")
+    public ResponseEntity<String> cancelSubscription(@RequestBody RequestCancelSubscription requestCancelSubscription){
+        log.info("구독 취소 API START");
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 엄격한 매칭
+
+        // 요청 userId 로 구독정보 조회 , 구독취소 사유 설정
+        SubscriptionDto subscriptionDto = subscriptionService.getSubscription(requestCancelSubscription.getUserId());
+        subscriptionDto.setCancelContent(requestCancelSubscription.getCancelContent());
+
+        subscriptionService.cancelSubscription(subscriptionDto);
+
+        log.info("구독 변경 API END");
+
+        return ResponseEntity.status(HttpStatus.OK).body("구독취소 완료");
+    }
+    
+    @ApiOperation(value = "구독 조회", notes = "특정 회원의 구독 정보를 조회한다.")
+    @GetMapping(value = "/subscription/{userId}")
+    public ResponseEntity<ResponseSubscription> getSubscription(@PathVariable("userId") String userId){
+        log.info("구독 조회 API START");
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 엄격한 매칭
+
+        SubscriptionDto subscriptionDto = subscriptionService.getSubscription(userId);
+
+        // dto -> response
+        ResponseSubscription responseSubscription = mapper.map(subscriptionDto, ResponseSubscription.class);
+
+        log.info("구독 조회 API END");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseSubscription);
     }
 }
--- BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/jpa/SubscriptionRepository.java
+++ BackEnd/subscription-service/src/main/java/com/example/subscriptionservice/jpa/SubscriptionRepository.java
@@ -1,4 +1,13 @@
 package com.example.subscriptionservice.jpa;
 
-public interface SubscriptionRepository {
+import com.example.subscriptionservice.entity.SubscriptionEntity;
+import com.example.subscriptionservice.entity.SubscriptionGradeEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Long> {
+    SubscriptionEntity findByUserId(String userId);
+
+    SubscriptionEntity findBySubId(long subId);
 }
