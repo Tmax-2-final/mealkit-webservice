69503e8cff730b73ba440a82d2781af9e1da3297
couleurtopaz <couleurtopaz@gmail.com>
2021-10-25 21:26:15 +0900

    리뷰 CRUD 구현

---
+++ BackEnd/review-service/src/main/java/com/example/reviewservice/jpa/ReviewRepository.java
@@ -0,0 +1,21 @@
+package com.example.reviewservice.jpa;
+
+import com.example.reviewservice.entity.ReviewEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+
+public interface ReviewRepository extends CrudRepository<ReviewEntity, Long> {
+
+    ReviewEntity findByReviewId(Long reviewId);
+    ReviewEntity findAllByOrderTypeAndPkgId(Integer orderType, Long pkgId);
+    ReviewEntity findAllByOrderTypeAndProductId(Integer orderType, Long productId);
+    Iterable<ReviewEntity> findAllByUserId(String userId);
+
+    ReviewEntity findByUserIdAndReviewId(String userId, Long reviewId);
+
+    @Transactional
+    void deleteByUserIdAndReviewId(String userId, Long reviewId);
+
+
+}
---
+++ BackEnd/review-service/src/main/java/com/example/reviewservice/vo/ResponseReview.java
@@ -0,0 +1,18 @@
+package com.example.reviewservice.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class ResponseReview {
+    private String title;
+    private String content;
+    private Integer rating;
+
+    private Long pkgId;
+    private Integer orderType;
+    private Long productId;
+    private String userId;
+    private Date modifiedAt;
+}
