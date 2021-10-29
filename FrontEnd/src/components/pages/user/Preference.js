689cc5c80418eeea1548f6a3255d6b3b583bc5c7
couleurtopaz <couleurtopaz@gmail.com>
2021-10-29 15:25:00 +0900

    Update Preference.js

--- FrontEnd/src/components/pages/user/Preference.js
+++ FrontEnd/src/components/pages/user/Preference.js
@@ -10,6 +10,31 @@ import axios from "axios";
 export default function Preference(props) {
 
 
+    const chkAgreeChangeHandler = (e) => {
+        if (!error) {
+            alert("테마는 두개만 선택 가능합니다.");
+            return;
+        } 
+
+        setState({
+            ...state,
+            [e.target.name]: e.target.checked,
+        });
+    };
+
+
+
+    const [state, setState] = useState({
+        chk1: false,
+        chk2: false,
+        chk3: false,
+        chk4: false,
+        chk5: false,
+    });
+
+
+    const { chk1, chk2, chk3, chk4, chk5 } = state;
+    const error = [chk1, chk2, chk3, chk4, chk5].filter((v) => v).length !== 2;
 
     return (
         <Fragment>
@@ -38,7 +63,7 @@ export default function Preference(props) {
                                                             <h5>고객님의 취향에 맞는 메뉴를 추천드리기 위한 용도로 사용됩니다.</h5><br /><br />
 
                                                             <div>
-                                                                <strong>1. 연령대를 골라주세요</strong><br /><br />
+                                                            <strong>1. 연령대를 골라주세요</strong><br /><br />
                                                                 <select>
                                                                     <option value="age">10대</option>
                                                                     <option value="age">20대</option>
@@ -57,11 +82,11 @@ export default function Preference(props) {
                                                             <br /><br />
                                                             <div>
                                                                 <strong>3. 좋아하는 테마를 두가지 골라주세요. </strong><br /><br />
-                                                                <input type="checkbox" name="teamname" value="Unlike" style={{ margin: "8px" }} /> 한식
-                                                                <input type="checkbox" name="teamname" value="little" style={{ margin: "8px" }} /> 양식
-                                                                <input type="checkbox" name="teamname" value="medium" style={{ margin: "8px" }} /> 중식
-                                                                <input type="checkbox" name="teamname" value="like" style={{ margin: "8px" }} /> 일식
-                                                                <input type="checkbox" name="teamname" value="like" style={{ margin: "8px" }} /> 동남아 음식
+                                                                <input type="checkbox" name="chk1" value="Unlike" checked={chk1} onChange={chkAgreeChangeHandler} style={{ margin: "8px" }} /> 한식
+                                                                <input type="checkbox" name="chk2" value="little" checked={chk2} onChange={chkAgreeChangeHandler} style={{ margin: "8px" }} /> 양식
+                                                                <input type="checkbox" name="chk3" value="medium" checked={chk3} onChange={chkAgreeChangeHandler} style={{ margin: "8px" }} /> 중식
+                                                                <input type="checkbox" name="chk4" value="like" checked={chk4} onChange={chkAgreeChangeHandler} style={{ margin: "8px" }} /> 일식
+                                                                <input type="checkbox" name="chk5" value="like" checked={chk5} onChange={chkAgreeChangeHandler} style={{ margin: "8px" }} /> 동남아 음식
                                                             </div>
                                                             <br /><br />
                                                             <div>
