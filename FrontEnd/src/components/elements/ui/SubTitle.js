0c2612d900c69279df7730321921ead0c30ba1c8
ojiwoong <ojiwoong@gmail.com>
2021-11-05 09:43:44 +0900

    DaumPost, Sidebar, SideMenu, SubTtile 수정, Title2 추가

--- FrontEnd/src/components/elements/ui/SubTitle.js
+++ FrontEnd/src/components/elements/ui/SubTitle.js
@@ -4,7 +4,7 @@ export default function SubTitle({ title }) {
 
     return (
         <div className="text-left mb-20 ">
-            <h2>{title}</h2>
+            <h2 style={{fontWeight:"bold"}}>{title}</h2>
             <p className=""></p>
         </div>
     );
