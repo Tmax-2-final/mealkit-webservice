a0042ed74af9a42d5b4bdd11dd993e734939c672
ojiwoong <ojiwoong@gmail.com>
2021-11-11 13:04:49 +0900

    로고 사이즈 변경 및 위치 조정

--- FrontEnd/src/components/elements/ui/Logo.js
+++ FrontEnd/src/components/elements/ui/Logo.js
@@ -1,10 +1,10 @@
 export default function Logo(){
 
     return(
-        <div className="col-xl-2 col-lg-2 col-md-6 col-4">
-            <div className="logo">
+        <div className="col-xl-2 col-lg-2 col-md-6 col-4 my-auto">
+            <div className="logo my-auto">
 
-                <a href="/"><img alt="" width="100%" src="/assets/img/logo/mailkitlogo.png" /></a>
+                <a href="/"><img alt="" width="100%" src="/assets/img/logo/mailkitlogo2.png" /></a>
 
             </div>
         </div>
