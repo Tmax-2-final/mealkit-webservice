a54c06ab8c10cc04493d26b27a5002273d204e6e
wmin401 <52664613+wmin401@users.noreply.github.com>
2021-11-15 21:33:41 +0900

    commit woonggi

--- Frontend-admin/src/views/productlist/Product.js
+++ Frontend-admin/src/views/productlist/Product.js
@@ -14,6 +14,7 @@ const Product = (props) => {
     const [startDate, setStartDate] = useState(new Date("2021/01/01"));
     const [endDate, setEndDate] = useState(new Date());
 
+    const [search, setSearch] = useState("");
     const [searchType, setSearchType] = useState("all");
     const [searchValue, setSearchValue] = useState("");
 
@@ -34,7 +35,7 @@ const Product = (props) => {
         }
 
         let token = localStorage.getItem('token');
-        const result = axios.get(`/catalog-service/catalogs`,{
+        const result = axios.get(`/catalog-service/catalogs?page=`,{
             headers: {
                 Authorization: `Bearer ${token}`
             }
@@ -74,97 +75,41 @@ const Product = (props) => {
         return result;
     }
 
-    const searchTypeChange = (e) => {
-        e.preventDefault();
-        setSearchType(e.target.value);
-    }
-
-    const searchValueChange = (e) => {
-        e.preventDefault();
-        setSearchValue(e.target.value);
-    }
-
-    const typeHandler = (type, pageNum, e) => {
-        e.preventDefault();
-        setLoading(true);
-
-        console.log(type);
-
-        let token = localStorage.getItem('token');
-
-        const start = moment(startDate, 'YYYY-MM-DD').format().split('T')[0];
-        const end = moment(endDate, 'YYYY-MM-DD').format().split('T')[0];
-
-        console.log(start)
-        console.log(end)
-
-    //     axios.get(`/subscription-service/subscription/status/${type}?page=${pageNum}&startDate=${start}&endDate=${end}`, {
-    //         headers: {
-    //             Authorization: `Bearer ${token}`
-    //         }
-    //     })
-    //         .then((res) => {
-    //             console.log(res.data);
-    //             if(res.status === 200) {
-    //                 setSubscriptionDatas(res.data.content);
-    //                 setTotalPages(res.data.totalPages);
-    //                 setCurrentPages(res.data.number + 1);
-    //                 setWhatPages(1);
-    //                 setCodeType(type);
-    //                 setLoading(false);
-    //             }
-    //             else {
-    //                 alert('오류가 발생했습니다.');
-    //             }
-    //
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //             alert('오류가 발생했습니다');
-    //         })
-     }
-
     const searchHandler = (pageNum, e) => {
         e.preventDefault();
         console.log(pageNum);
         setLoading(true);
 
-        let token = localStorage.getItem('token');
+      let token = localStorage.getItem('token');
 
-        const start = moment(startDate, 'YYYY-MM-DD').format().split('T')[0];
-        const end = moment(endDate, 'YYYY-MM-DD').format().split('T')[0];
+      let body = {
+        searchData: search
+      }
 
-        console.log(start)
-        console.log(end)
+      axios.post(`/catalog-service/catalogs/search?page=${pageNum}`, body, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+        .then((res) => {
+          console.log(res);
+          if(res.status === 200){
+            setUserDatas(res.data.content);
+            setTotalPages(res.data.totalPages);
+            setCurrentPages(res.data.number + 1);
+            setWhatPages(1);
+            setLoading(false)
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          alert('오류가 발생했습니다');
+        })
 
-        console.log(searchType)
-        console.log(searchValue)
 
-        // const result = axios.get(`/subscription-service/subscription/keyword/search?` +
-        //     `page=${pageNum}&searchType=${searchType}&searchValue=${searchValue}` +
-        //     `&startDate=${start}&endDate=${end}`,
-        //     {
-        //         headers: {
-        //             Authorization: `Bearer ${token}`
-        //         }
-        //     })
-        //     .then((res) => {
-        //         console.log(res.data);
-        //         if (res.status === 200) {
-        //             setSubscriptionDatas(res.data.content);
-        //             setTotalPages(res.data.totalPages);
-        //             setCurrentPages(res.data.number + 1);
-        //             setWhatPages(2);
-        //             setLoading(false);
-        //         }
-        //         else {
-        //             alert('오류가 발생했습니다.');
-        //         }
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //         alert('오류가 발생했습니다');
-        //     })
+
+
+
 
     }
 
