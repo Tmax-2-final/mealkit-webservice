a6045187197b4d9a7dfc1dde04fb021358c470cd
ojiwoong <ojiwoong@gmail.com>
2021-11-26 01:35:02 +0900

    Update WebSecurity.java

--- BackEnd/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ BackEnd/user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -53,7 +53,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                         "or hasIpAddress('192.168.201.144') or hasIpAddress('172.30.1.55') " +
                         "or hasIpAddress('10.10.20.77') or hasIpAddress('192.168.0.100')" +
                         "or hasIpAddress('192.168.219.100') or hasIpAddress('172.30.1.32')" +
-                        "or hasIpAddress('192.168.95.44') or hasIpAddress('192.168.201.126')")                                   // ip addr
+                        "or hasIpAddress('192.168.95.44') or hasIpAddress('192.168.201.126')" +
+                        "or hasIpAddress('15.165.139.232')")                                   // ip addr
 //                .hasIpAddress(env.getProperty("gateway.ip")) // 172.18.0.5
                 .and()
                 .addFilter(getAuthenticationFilter());
